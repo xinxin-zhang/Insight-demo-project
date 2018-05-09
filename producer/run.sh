b9ae52e96551f414787b386ce5f406d0d4a8a61a
Ubuntu <ubuntu@ip-10-0-0-14.us-west-2.compute.internal>
2018-05-09 22:16:00 +0000

    updates

--- producer/run.sh
+++ producer/run.sh
@@ -1,1 +1,1 @@
-bash spawn_kafka_streams.sh ec2-35-160-167-99.us-west-2.compute.amazonaws.com 4 k1
+python runTest.py
--- producer/runTest.py
+++ producer/runTest.py
@@ -1,4 +1,26 @@
-#/bin/bash
+import sys
+from random import Random
+import unittest
+import producer
 
-cd "$(dirname "$0")"
-python2.7 test/test.py
+
+class ProducerTest(unittest.TestCase):
+
+    def setUp(self):
+        global random
+        random = Random(0)
+
+
+    def test_observation(self):
+        p = producer.Producer()
+        p.produce_msgs(0)
+        self.assertEqual(p.observationgroup_field, 50)
+        self.assertEqual(p.observationorder_field, 5)
+        self.assertEqual(p.frequency_field, 4205.71580830845)
+        self.assertEqual(p.snr_field, 25.891675029296334)
+        self.assertEqual(p.driftrate_field, 0.10634058391819423)
+        self.assertEqual(p.uncorrectedfrequency_field, 4206.1962941714055)
+
+
+if __name__ == "__main__":
+    unittest.main()
