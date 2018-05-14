46bfc5b81d3a77c0ab9163a5f0ddf95f319fa04b
Ubuntu <ubuntu@ip-10-0-0-14.us-west-2.compute.internal>
2018-05-14 21:50:24 +0000

    go

--- producer/run.sh
+++ producer/run.sh
@@ -1,3 +1,3 @@
 #echo $PYTHONPATH
 /usr/bin/python2 /home/ubuntu/Insight-demo-project/producer/runTest.py
-chmod 666 kafka_producer.pyc
+rm /home/ubuntu/Insight-demo-project/producer/kafka_producer.pyc
