d83ceb695f7df5c3b8125b91b67a767eca4ac60d
Ubuntu <randomwalker2@gmail.com>
2018-05-16 03:32:32 +0000

    add

--- pull.sh
+++ pull.sh
@@ -7,6 +7,8 @@ tmux kill-session -t k1
 cd /usr/local/spark/sbin/
 ./stop-all.sh
 sleep 20s
+cqlsh -e "TRUNCATE hitplayground.hitinfo ;"
+cqlsh -e "TRUNCATE hitplayground.groupinfo ;"
 ./start-all.sh
 cd /home/ubuntu/Insight-demo-project/spark-streaming
 sbt assembly
