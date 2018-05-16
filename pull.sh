25823f562f546e2343c270bccb950035ecd1babd
Ubuntu <randomwalker2@gmail.com>
2018-05-16 21:48:38 +0000

    go

--- pull.sh
+++ pull.sh
@@ -13,7 +13,7 @@ sleep 1m
 cd /home/ubuntu/Insight-demo-project/spark-streaming
 sbt assembly
 sleep 2m
-spark-submit --class PriceDataStreaming --master spark://ec2-35-160-167-99.us-west-2.compute.amazonaws.com:7077 --jars target/scala-2.11/streamhits-assembly-1.0.jar target/scala-2.11/streamhits-assembly-1.0.jar
+spark-submit --class PriceDataStreaming --master spark://ec2-35-160-167-99.us-west-2.compute.amazonaws.com:7077 --jars target/scala-2.11/streamhits-assembly-1.0.jar target/scala-2.11/streamhits-assembly-1.0.jar &
 sleep 30s
 cd /home/ubuntu/Insight-demo-project/producer/
 bash spawn_kafka_streams.sh ec2-35-160-167-99.us-west-2.compute.amazonaws.com 4 k1
