fadf4786e6e758b6b099f5cb9a98717fa3e6ac88
Ubuntu <randomwalker2@gmail.com>
2018-05-16 03:50:06 +0000

    update

--- pull.sh
+++ pull.sh
@@ -12,4 +12,8 @@ cqlsh -e "TRUNCATE hitplayground.groupinfo ;"
 ./start-all.sh
 cd /home/ubuntu/Insight-demo-project/spark-streaming
 sbt assembly
-sleep 1m
+sleep 2m
+spark-submit --class PriceDataStreaming --master spark://ec2-35-160-167-99.us-west-2.compute.amazonaws.com:7077 --jars target/scala-2.11/streamhits-assembly-1.0.jar target/scala-2.11/streamhits-assembly-1.0.jar
+sleep 30s
+cd /home/ubuntu/Insight-demo-project/producer/
+bash spawn_kafka_streams.sh ec2-35-160-167-99.us-west-2.compute.amazonaws.com 4 k1
